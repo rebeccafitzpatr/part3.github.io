94e78ba5132a9b98742501dc109db0dca3a34736
rebeccafitzpatr <rfit696@aucklanduni.ac.nz>
2024-03-08 23:58:10 +1300

    added extra notes page, trying to build up the nav bar

---
+++ src/Linguist.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+function Linguist() {
+    return (
+        <h3>These are LINGUIST100 notes for 2024</h3>
+    )
+}
+
+export default Linguist;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <ul>
@@ -6,9 +7,9 @@ function Navbar() {
             <li><h4>SOFTENG351</h4></li>
             <li><h4>SOFTENG370</h4></li>
             <li><h4>COMPSCI373</h4></li>
-            <li><h4>LINGUIST100</h4></li>
+            <li><Link to="/linguist"><h4>LINGUIST100</h4></Link></li>
         </ul>
-    )
+    );
 }
 
 export default Navbar;
