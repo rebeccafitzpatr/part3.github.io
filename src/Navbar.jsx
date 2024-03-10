f56cdd135541582a11ea7986381e3a0d7c295d15
rebeccafitzpatr <rfit696@aucklanduni.ac.nz>
2024-03-10 22:36:36 +1300

    fixed a bug with the linguist page not found

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
             <li><h4>SOFTENG351</h4></li>
             <li><h4>SOFTENG370</h4></li>
             <li><h4>COMPSCI373</h4></li>
-            <li><Link to="/linguist"><h4>LINGUIST100</h4></Link></li>
+            <li><a href= "<Linguist />"><h4>LINGUIST100</h4></a></li>
         </ul>
     );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
-import Linguist from "./Linguist";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//import Linguist from "./Linguist";
 
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />
-    },
-    {
-      path: "/linguist",
-      element: <Linguist />
-    }
-]);
 ReactDOM.render(
     //<h1>Hello, world!</h1>,
     <App />,
     
     document.getElementById('app'),
-    <RouterProvider router={router} />
 );
