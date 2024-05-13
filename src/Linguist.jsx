68c02babb84dd670bfde954eeb228ecf49f7a2a4
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2024-05-13 22:32:29 +0000

    adding to linguist page

--- src/Linguist.jsx
+++ src/Linguist.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 function Linguist() {
     return (
-        <h3>These are LINGUIST100 notes for 2024</h3>
+        <article>
+            <article>
+                <h3>These are LINGUIST100 notes</h3>
+            </article> 
+            <article>
+                <h2>Linguistic diversity</h2>
+                <p> </p>
+            </article>
+        </article>
     )
 }
 
