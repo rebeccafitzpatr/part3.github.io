45117ea1dfdb3273422124797a72c198c832ee07
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2024-05-15 11:50:19 +0000

    added notes on linguistic diversit

--- src/Linguist.jsx
+++ src/Linguist.jsx
@@ -7,7 +7,10 @@ function Linguist() {
             </article> 
             <article>
                 <h2>Linguistic diversity</h2>
-                <p> </p>
+                <p>There are about 7000 estimated languages in the world, however it is hard to have a precise number due to the blur between a 'language' and a 'dialect'. This linguistic diversity is caused when human populations become isolated from their neighbours. </p>
+                <p>Some regions are more linguistically diverse than others.</p><br/>
+                <p>However diversity today is diminishing; the number of languages is always decreasing. This is especially as we find increased mobility and access to different areas of the would; people tend to aim to learn the most socioeconomically valuable variety of language. Furthermore, political messaging can lead to preconceived ideologies about language, which are poorly based.</p> 
+                <p>There are many reasons to learn a popular language as well as smaller languages, these decisions do not need to be an either or.</p>
             </article>
         </article>
     )
