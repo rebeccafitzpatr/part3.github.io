b731662b907c6d61e7bc8b420c64d98476d6deae
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2024-05-23 11:58:32 +0000

    added syntax notes

--- src/Linguist.jsx
+++ src/Linguist.jsx
@@ -12,6 +12,12 @@ function Linguist() {
                 <p>However diversity today is diminishing; the number of languages is always decreasing. This is especially as we find increased mobility and access to different areas of the would; people tend to aim to learn the most socioeconomically valuable variety of language. Furthermore, political messaging can lead to preconceived ideologies about language, which are poorly based.</p> 
                 <p>There are many reasons to learn a popular language as well as smaller languages, these decisions do not need to be an either or.</p>
             </article>
+            <article>
+                <h2>SYNTAX - the study of the accepted order of words in a language's sentences (traditionally called grammar)</h2>
+                <p>Languages have infinite potential for creativity and variation; this lends to the fact that language rules are not permanent. Furthermore syntax should not be confused with prescriptive rules or the assertion of what is 'proper' in grammar. Linguistic study is focused on how language is used, whether or not it is proper is not vital.</p>
+                <br/> <p><b>Word Classes:</b> All words in any language are either open class or closed class</p>
+            </article>
+
         </article>
     )
 }
