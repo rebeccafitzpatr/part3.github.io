1dac9d91da82953d4dd20b80267f3ff7ed30117c
Rebecca Fitzpatrick <rebecca.fitzpatr@gmail.com>
2024-06-07 05:00:24 +0000

    added OS notes

--- src/Se370.jsx
+++ src/Se370.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 function Se370() {
     return (
+        <article>
+
+
         <h3>These are SOFTENG370 notes for 2024</h3>
+
+        <ul>
+            <li>Introduction</li>
+            <li>Virtual Machines, Assignment 1 and Process Creation</li>
+            <li>Processes + Threads</li>
+            <li>Concurrency and Deadlocks</li>
+            <li>CPU Scheduling and Deadlock and Assignment 2</li>
+        </ul>
+
+        <h4>Introduction to OS </h4>
+        <p>An OS is a special software that acts as the intermediary between the user applications of the computer and the computer hardware. The purpose of an operating system is to provede an environment where the user can execute programs in a convenient and efficient manner.</p> <br/>
+        <p>OS achieves this by abstracting specific hardware details such as specific sound cards or disks, thereby simplifying communication between software applications and the underlying hardware components.OS also provides commonly used services i.e file management, networking, process management</p> <br/>
+
+        <p>Why study OS? A computer system can be divided into four components: hardware, opereationsyste, applications programs and users. Or in other words hardware, software and data. Operating systems provide the means for proper use of resources in the operation of the computer system - similar to the government of the computer, OS doesn't provide anything on its own, but rather creates the environment for meaningful work to be done.
+        </p> <br/>
+
+        <p> </p>
+        
+        </article>
     )
 }
 
