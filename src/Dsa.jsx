2deb800fbb963584a360108865838c9f161f4074
rebeccafitzpatr <rfit696@aucklanduni.ac.nz>
2024-06-26 15:10:59 +1200

    added week 2 DSA notes

--- src/Dsa.jsx
+++ src/Dsa.jsx
@@ -14,6 +14,15 @@ function Dsa() {
         <h3>Binary search</h3>
         <p>Problem: Given an array of integers A and an integer x, find an index of x in A or return -1 if x is not in A. If we don't know anything about A, we have no choice but to go through the entire array. <i>O(n)</i> Given a <b>sorted</b> array of integers A and an integer x, find an index of x in A or return -1 if x is not in A. <i>O(log n)</i>. What about: Given a <b>sorted</b> array of integers A and an integerx, find the <b>first</b> index of x in A or return -1 if x is not in A. Binary search can be tricky to get right. Find the <b>last</b> index? <b>Exercises:</b> 1. Suppose that an input array A is first going up then down. Write an algorithm that outputs the largest number in A. 2. Given a sorted array A and two numbers Low and Hi, write an algorithm that outputs how many numbers in A are greater than or equal to Low and less than or equal to Hi.</p>
 
+        <h3>Week 2: Sorting</h3>
+        <p>Problem: Sort a given array A in non-decreasing order. Two approaches Selection sort vs insertion sort. Both are O(n^2) in the worst case, but selection sort is a bit easier to implement. Insertion sort can be faster if the array is almost sorted.</p>
+        <br/>
+        <p>Divide and conquer - merge sort can sort the given array in non-decreasing order with time complexity O(nlog n). Makes use of the magic word <b>Recursion</b>. At most C n log n operations. With mergesort, it is easy to divide the problem into smaller pieces, then solve each subproblem using recursion but it is difficult to combine the solutions. In contrast <b>Quicksort</b> dividing the problem into smaller pieces can be difficult, while combining the solutions is easy. <b>Exercises for analysis, use the tree method to get a feeling for what the complexity is, Induction to formally prove it. Challenge - design an O(n logn) divide and conquer algorithm for the largest welcome sign problem.</b></p>
+        <br/>
+        <p>Quicksort idea: Take the last element, and partition the array around it. Time complexity = worst case: theta(n2), there exists theta(nlogn) solution. Average case with random pivot = O(nlogn) <b>Welcome to the world of randomised algorithms</b></p>
+
+        <h3>Week 3: Quick Select</h3>
+
         </article>
     )
 }
